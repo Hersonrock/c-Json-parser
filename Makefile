e7cc4ab8eccb20a1df54335382b4c2d2a3b879ee
Hersonrock <hjpg20@gmail.com>
2024-01-04 11:07:01 -0500

    Json parser initial commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+CFLAGS=-Wall -g
+
+all:	json-parser
+
+clean:
+	rm -r json-parser
---
+++ json-parser.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+        printf("Hello, Json\n");
+        return 0;
+}
