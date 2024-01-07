1dfc64df4148aa32e4b66769d730e4d0cf2e0b9f
Hersonrock <hjpg20@gmail.com>
2024-01-07 09:27:31 -0500

    Created ProofOfConcepts Directory and completes the POC for dinamically allocated linked lists. Valgrind check passed for memory leaks

--- poc/poc_linked_list.c
+++ poc/poc_linked_list.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 
 typedef struct node{
@@ -6,7 +7,7 @@ typedef struct node{
         struct node *next;
 }node_t;
 
-void printlist(node_t *head){
+void printList(node_t *head){
         
         node_t *temp = head;
 
@@ -16,22 +17,53 @@ void printlist(node_t *head){
         }
         printf("\n");
 }
+
+node_t *createNewNode(int value){
+
+        node_t *ptr = malloc(sizeof(node_t));
+        ptr->value = value;
+        ptr->next = NULL;
+
+        return ptr;
+}
+
+node_t *createChain(int n){
+        node_t *head;
+        node_t *step;
+        node_t *tmp;
+
+        int i = 0;
+
+        tmp = createNewNode(i);
+        head = tmp;
+        step = tmp;
+
+        for( i = 1 ; i < n; i++){
+                tmp = createNewNode(i);
+                step->next = tmp;
+                step = tmp;
+        }
+
+        return head;
+}
+
+void freeMemory(node_t *head){
+        node_t *tmp;
+
+        while (head != NULL){
+                tmp = head;
+                head = head-> next;
+                free(tmp);
+        }
+}
+
 int main(int argc, char *argv[]){
-
-        node_t a1, b1, c1;
-        node_t *head;        
-
-        a1.value = 3;
-        b1.value = 7;
-        c1.value = 15;
-
-        //linkage
-
-        head = &a1;
-        a1.next = &b1;
-        b1.next = &c1;
-        c1.next = NULL;
         
-        printlist(head);
+        node_t *head;
+
+        head = createChain(20);
+        printList(head);
+        freeMemory(head);
+
         return 0;
 }
