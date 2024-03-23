04c4043d5ad1b607c76ffffa2f697c71c0b448d8
Hersonrock <hjpg20@gmail.com>
2024-03-23 01:12:53 -0500

    improves poc, changes names, fixes print- Working model!

--- poc/poc_tree.c
+++ poc/poc_tree.c
@@ -5,67 +5,64 @@
 
 union anyNode;
 
-//indexNode(JSON object) will store object keys as well as the pointers to the values
-//or the head of new indexNode (Json Object)
-typedef struct indexNode{
+//objectNode(JSON object) will store object keys as well as the pointers to the values
+//or the head of new objectNode (Json Object)
+struct objectNode{
         char *key;
         union anyNode *step;
-        struct indexNode *next;
-}indexNode_t;
+        struct objectNode *next;
+};
 
-//nodes will store any value 
+//arrayNodes will store any value 
 //strings, doubles, literals and lists.
-typedef struct node{
-        union anyNode *value;    
-        struct node *next; 
-}node_t;
+struct arrayNode{
+        union anyNode *step;    
+        struct arrayNode *next; 
+};
 
 union anyNode{
-        indexNode_t *index_u;
-        node_t *node_u;
-        char *value_u;
+        struct objectNode *object_u;
+        struct arrayNode *array_u;
+        char *value;
 };
 //debug function to create random strings of random lenghts
 char *stringCreate(){
-        int n = rand() %10 + 1;
+        int n = rand() %5 + 2;
         char *string = (char *)malloc((size_t)n);
         for(int  i = 0 ; i < n-1; i++){
-                int r = rand() %25 +66;
+                int r = rand() %25 +97;
                 string[i] = (char)r ;
         }
         string[n-1] = '\0';
         return string;
 }
 
-node_t *createNode(){
+struct arrayNode *createArrayNode(){
 
-        node_t *ptr = malloc(sizeof(node_t));
-        ptr->value = malloc(sizeof(union anyNode));
+        struct arrayNode *ptr = malloc(sizeof(struct arrayNode));
+        ptr->step = malloc(sizeof(union anyNode));
 
-        char *str = stringCreate();
-        
-        ptr->value->value_u = malloc(sizeof(str));
-        strcpy(ptr->value->value_u, str);
+        ptr->step->value = stringCreate(); 
         ptr->next = NULL;
 
         return ptr;
 }
 
-//Returns the head of n-nodes chain.
-node_t *createNodeChain(int n){
-        node_t *head;
-        node_t *step;
-        node_t *tmp;
+//Returns the head of n-arrayNodes chain.
+struct arrayNode *createArrayNodeChain(int n){
+        struct arrayNode *head;
+        struct arrayNode *step;
+        struct arrayNode *tmp;
 
         int i = 0;
 
-        tmp = createNode();
+        tmp = createArrayNode();
         head = tmp;
         step = tmp;
 
         for( i = 1 ; i < n; i++){
-                tmp = createNode();
-                step->next = tmp;
+                tmp = createArrayNode();
+                step->next= tmp;
                 step = tmp;
         }
 
@@ -73,27 +70,27 @@ node_t *createNodeChain(int n){
 }
 
 //Creates IndexNode
-indexNode_t *createIndexNode(int n){
-        indexNode_t *ptr = malloc(sizeof(indexNode_t));
+struct objectNode *createObjectNode(int n){
+        struct objectNode *ptr = malloc(sizeof(struct objectNode));
         ptr->key = stringCreate();
         ptr->step = malloc(sizeof(union anyNode));
-        ptr->step->node_u = createNodeChain(n);
+        ptr->step->array_u = createArrayNodeChain(n);
         ptr->next = NULL;
         return ptr;
 }
 
-indexNode_t *createIndexNodeChain(int n, int n2){
-        indexNode_t *head;
-        indexNode_t *step;
-        indexNode_t *tmp;
+struct objectNode *createObjectNodeChain(int n, int n2){
+        struct objectNode *head;
+        struct objectNode *step;
+        struct objectNode *tmp;
         int i = 0;
 
-        tmp = createIndexNode(n);
+        tmp = createObjectNode(n);
         head = tmp;
         step = tmp;
 
         for( i = 1 ; i < n2; i++){
-                tmp = createIndexNode(n);
+                tmp = createObjectNode(n);
                 step->next = tmp;
                 step = tmp;
         }
@@ -102,33 +99,38 @@ indexNode_t *createIndexNodeChain(int n, int n2){
 }
 
 /* Disabling for the moment, needs updating
-void freeMemory(node_t *head){
+void freeMemory(struct arrayNode *head){
         listNode_t *tmp;
 
         while (head != NULL){
                 tmp = head;
                 head = head-> next;
                 free(tmp->key);
-                free(tmp->value);
+                free(tmp->step);
                 free(tmp);
         }
 }
 */
 
-void printObjects(indexNode_t *head){
+void printObjects(struct objectNode *head){
 
-        node_t *tmp = head->step->node_u;
+        struct arrayNode *tmp = head->step->array_u;
         
-        printf("{\n\t");
+        printf("{\n");
          
         while ( head->next != NULL){
                 printf("\"%s\": [", head->key);
-                while( tmp->next != NULL){
-                        printf("%s,", head->step->node_u->value->value_u);
+                while ( tmp->next != NULL){
+                        
+                        printf("%s", tmp->step->value);
                         tmp = tmp->next;
+                        if ( tmp->next != NULL){
+                                printf(",");
+                        }
                 }
                 printf("]\n");
                 head = head->next;
+                tmp = head->step->array_u;
         }
         printf("}\n");
 
@@ -137,9 +139,9 @@ void printObjects(indexNode_t *head){
 int main(int argc, char *argv[]){
         
         srand(time(NULL));
-        indexNode_t *head;
+        struct objectNode *head;
 
-        head = createIndexNodeChain(6, 4);
+        head = createObjectNodeChain(6, 4);
         printObjects(head);
 //        freeMemory(head);
 
